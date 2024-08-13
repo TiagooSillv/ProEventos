2b4cf4b03d5a06acc9893aae5c53c2032b8806e7
Tiago Silva <tiago.sillv@gmail.com>
2024-08-12 22:42:48 -0300

    feat : Foi feito o Mapeamento do banco de dados usando o ORM Entity FrameWork

--- Controllers/EventoController.cs
+++ Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.Data;
 using ProEventos.Models;
 using System.Collections.Generic;
 
@@ -10,38 +11,26 @@ namespace ProEventos.Controllers
     {
         private readonly IEnumerable<Evento> _evento = new List<Evento>
         {
-            new Evento
-            {
-                EventoId = 1,
-                Tema = "Angular e .Net",
-                Local = "Es",
-                Lote = "1 lote",
-                Quantidade = 10,
-                DataEvento = DateTime.Now.AddDays(2),
-                ImageURL = "Foto.png"
-            },
-            new Evento
-            {
-                EventoId = 2,
-                Tema = "React e Node.js",
-                Local = "RJ",
-                Lote = "2 lote",
-                Quantidade = 20,
-                DataEvento = DateTime.Now.AddDays(5),
-                ImageURL = "Foto2.png"
-            }
+           
         };
 
         [HttpGet]
         public IEnumerable<Evento> GetAll()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
         public Evento GetById(int id)
         {
-            return _evento.FirstOrDefault(e => e.EventoId == id);
+            return _context.Eventos.FirstOrDefault(e => e.EventoId == id);
+        }
+
+        private readonly DataContext _context;
+
+        public EventoController (DataContext context)
+        {
+            _context = context;
         }
     }
 }
---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProEventos.Models;
+
+namespace ProEventos.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Evento> Eventos { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> option) : base(option){}
+    }
+}
